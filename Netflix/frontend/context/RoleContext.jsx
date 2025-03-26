7c51c201201223a2710f3d190a7dfd912e89f6ce
Ayush Shrivastava <113074497+AyushShrivastava14@users.noreply.github.com>
2025-03-27 00:08:44 +0530

    Update RoleContext.jsx

--- Netflix/frontend/context/RoleContext.jsx
+++ Netflix/frontend/context/RoleContext.jsx
@@ -22,7 +22,7 @@ export const RoleProvider = ({ children }) => {
 
   const saveRole = async (role) => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/generate-token", {
+      const response = await fetch("https://mailfetcher-backend.onrender.com/generate-token", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ role: role }),
@@ -50,7 +50,7 @@ export const RoleProvider = ({ children }) => {
   const verifyStoredToken = async (role, storedToken) => {
     if (storedToken) {
       try {
-        const response = await fetch("http://127.0.0.1:5000/verify-token", {
+        const response = await fetch("https://mailfetcher-backend.onrender.com/verify-token", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ token: storedToken }),

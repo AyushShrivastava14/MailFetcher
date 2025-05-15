677b899994dca499403f71d54ae5f25f5969bc47
Ayush Shrivastava <ayush.shrivastava.023@gmail.com>
2025-05-15 17:55:09 +0530

    fixed url bug

--- Admin/client/context/Context.jsx
+++ Admin/client/context/Context.jsx
@@ -3,8 +3,8 @@ import React, { createContext, useContext } from "react";
 const RoleContext = createContext("");
 
 export const RoleProvider = ({ children }) => {
-  // const url = import.meta.env.VITE_URL;
-  const url = "http://127.0.0.1:5000";
+  const url = import.meta.env.VITE_URL;
+  // const url = "http://127.0.0.1:5000";
 
   return (
     <RoleContext.Provider
